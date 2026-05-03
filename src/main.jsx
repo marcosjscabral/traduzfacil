543d634926581726ca26c378333a7cb93afe1f26
marcos <marcosjscabral@gmail.com>
2026-05-03 17:07:41 -0300

    atulizacao_manifest_4

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/pwabuilder-sw.js')
+      .then(reg => console.log('SW registrado com sucesso!', reg))
+      .catch(err => console.error('Erro ao registrar SW:', err));
+  });
+}
